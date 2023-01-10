c29e9f5975921f430848ffd0e6712132b122f794
kgopotso <kgopotso.maloma@dadevs.co.za>
2023-01-10 16:23:04 +0200

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import Dashboard from './components/dashboard/dashboard';
-import Home from "./components/home/home";
+
 
 function App() {
   return (
     <div data-theme='cupcake' >
       <button className='btn'></button>
     <Dashboard/>
-    <Home/>
+  
 
   </div>
 
