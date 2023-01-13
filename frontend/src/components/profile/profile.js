e44a20e1329e58dc1b78b58ea4838167ed3e43df
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2023-01-13 08:55:27 +0200

    strapi

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from 'react';
-// import Home from '../home/home';
+import Home from '../home/home';
 // import './dashboard.css';
 import './profile.css'
 
@@ -31,7 +31,7 @@ function Profile() {
     // This function updates the component with the
     // current todo data stored in the server
     function update() {
-        fetch(`http://localhost:1337/api/todosapi/todos`)
+        fetch(`${process.env.REACT_APP_BACKEND}api/todos`)
             .then(res => res.json())
             .then(firstName => {
                 setFirstName(firstName.data);
@@ -41,6 +41,32 @@ function Profile() {
                 setGender(gender.data);
             })
     }
+
+
+    // function changeTodo(e) {
+    //     e.preventDefault();
+    //     let item = newTodo;
+    //     let pos = todo.id;
+    //     let body = {
+    //       data: {
+    //         item
+    //       }
+    //     };
+    
+    //     fetch(`${process.env.REACT_APP_BACKEND}api/todos/${pos}`, {
+    //       method: "PUT",
+    //       headers: {
+    //         'Content-type': 'application/json'
+    //       },
+    //       body: JSON.stringify(body)
+    //     })
+    //       .then(() => {
+    //         setEdit(false);
+    //         update();
+    //       })
+    //   }
+
+
 
     // This function sends a new todo to the server
     // and then call the update method to update the
@@ -52,9 +78,10 @@ function Profile() {
             data: {
                 firstName: newFirstName,
                 lastName: newlastName,
-                email: newEmail,
+                newEmail: newEmail,
                 PhoneNumber: newPhoneNumber,
                 gender: newGender,
+
 
             }
         };
@@ -85,22 +112,17 @@ function Profile() {
 
                 <div class="card card-compact h-3/4 w-3/4 bg-base-200  shadow-xl">
 
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" className="profilepic " /></figure>
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" className="profilepic r" /></figure>
                     <div class="card-body">
 
-                        <p>First name:  </p>
-                        {/* {firstName.attributes.Country} */}
-                        <p>Last name:  </p>
-                        {/* {lastName.attributes.Country} */}
+                        <p>First name: </p>
+                        <p>Last name </p>
                         <p>Email: </p>
-                        {/* {email.attributes.Country} */}
-                        <p>Phone number:  </p>
-                        {/* {phoneNumber.attributes.Country}  */}
-                        <p>Gender:  </p>
-                        {/* {gender.attributes.Country}  */}
+                        <p>Phone number: </p>
+                        <p>Gender: </p>
 
                         <div class="card-actions justify-end">
-                            <label htmlFor="my-modal-4" className="btn">Edit</label>
+                            <label htmlFor="my-modal-4" className="btn btn-primary">open modal</label>
                         </div>
                     </div>
                 </div>
@@ -115,26 +137,26 @@ function Profile() {
 
                         <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                         <div class="card-body">
-                            <form className="form" onSubmit={addFirstName}>   
-                                <input type="text" placeholder="First name" class="input input-bordered input-md w-full max-w-xs" value={newFirstName} onChange={e => setNewFirstName(e.currentTarget.value)} />
-
+                            <form className="form" >
+                                <p>Name: </p>
+                                <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" value={newFirstName} onChange={e => setNewFirstName(e.currentTarget.value)} />
                                 
-                                <input type="text" placeholder="Last name" class="input input-bordered input-md w-full max-w-xs" value={newlastName} onChange={e => setNewFirstName(e.currentTarget.value)} />
 
-                                
-                                <input type="text" placeholder="Email" class="input input-bordered input-md w-full max-w-xs" value={newEmail} onChange={e => setNewLastName(e.currentTarget.value)} />
-                   
-                                
-                                <input type="text" placeholder="Phone number" class="input input-bordered input-md w-full max-w-xs" value={newPhoneNumber} onChange={e => setNewEmail(e.currentTarget.value)} />
+                                <p>Email: </p>
+                                <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" value={newlastName} onChange={e => setNewLastName(e.currentTarget.value)} />
+                                <p>Phone number: </p>
+                                <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" value={newEmail} onChange={e => setNewEmail(e.currentTarget.value)} />
 
-                                
-                                <input type="text" placeholder="Gender" class="input input-bordered input-md w-full max-w-xs" value={newGender} onChange={e => setNewPhoneNumber(e.currentTarget.value)} />
+                                <p>Phone numbers: </p>
+                                <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" value={newPhoneNumber} onChange={e => setNewPhoneNumber(e.currentTarget.value)} />
 
-                
-                                <br />
-                                <br />
-                                <button type="submit" className="todo_button btn btn-secondary  btn btn-secondary">Save</button>
-                      
+                                <p>Gender: </p>
+                                <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" value={newGender} onChange={e => setNewGender(e.currentTarget.value)} />
+
+                                <br/><br/>
+                                <button class="btn btn-active btn-primary">Button</button> 
+                                <button className="todo_button" type="submit">Change todo</button>     
+
 
                             </form>
 
