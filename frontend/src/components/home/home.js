965d1fce2fc3a81b4ae6be4f5350b83f65fdc08f
Mankopodi Kgopotso <kgopotsomaloma170@gmail.com>
2023-01-16 09:47:09 +0200

    pres

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -7,7 +7,7 @@ function home() {
       <div>
         <div className="grid md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 lg:grid-cols-2 gap-3">
 
-          <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card w-96 bg-	secondary-focus text-primary-content mt-16">
             <div className="card-body text-center">
 
               <h2 className="card-title flex justify-center">Event Packages</h2>
@@ -18,7 +18,7 @@ function home() {
             </div>
           </div>
 
-          <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card w-96 bg-	secondary-focus text-primary-content mt-16">
             <div className="card-body ">
               <h2 className="card-title flex justify-center ">Review & Ratings</h2>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
@@ -28,7 +28,7 @@ function home() {
             </div>
           </div>
 
-          <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card w-96 bg-	secondary-focus text-primary-content mt-16">
             <div className="card-body">
 
               <h2 className="card-title flex justify-center ">Reports</h2>
@@ -40,7 +40,7 @@ function home() {
             </div>
           </div>
 
-          <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card w-96 bg-	secondary-focus text-primary-content mt-16">
             <div className="card-body">
               <h2 className="card-title flex justify-center ">Event Venue </h2>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
