53dd4afb5baf705b9207f0c42a51c141d68f0fad
shongie <116272145+shongie@users.noreply.github.com>
2023-01-16 08:52:14 +0200

    login ui

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -99,7 +99,7 @@ const config = require('./config.json')
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-        <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+        <h1 className="text-3xl font-semibold text-center text-gray-700 underline">
           Sign in
         </h1>
         <form className="mt-6" onSubmit={login}>
@@ -131,11 +131,11 @@ const config = require('./config.json')
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-          <Link  to={'/forgot-password'} className="text-xs text-purple-600 hover:underline">
+          <Link  to={'/forgot-password'} className="text-xs text-black-900 hover:underline">
             Forget Password?
           </Link>
           <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">
               Login
             </button>
           </div>
@@ -144,7 +144,7 @@ const config = require('./config.json')
         <p className="mt-8 text-xs font-light text-center text-gray-700">
           {" "}
           Don't have an account?{" "}
-          <a href="#" className="font-medium text-purple-600 hover:underline">
+          <a href="#" className="font-medium text-black-900 hover:underline">
             Sign up
           </a>
         </p>
